8ebaef2eb4d7e9662042cb9b1d0e45083bfd685a
fllantada <franciscollantada@gmail.com>
2022-11-24 00:05:44 -0300

    Swagger added

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
 } from '@nestjs/common';
 import { GameService } from './game.service';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateGameDto } from './dto/create-game.dto';
 
 @Controller('game')
@@ -15,14 +16,21 @@ export class GameController {
   constructor(private readonly gameService: GameService) {}
 
   //start new game
+  @ApiTags('New Game')
   @Post('/newGame')
   findAll(@Body() createGameDto: CreateGameDto) {
     return this.gameService.createNewGame(createGameDto);
   }
 
   //spin new game
+  @ApiTags('Play')
   @Get('/play/:id')
   findOne(@Param('id') id: string) {
     return this.gameService.play(id);
+  }
+  @ApiTags('Checkout')
+  @Get('/checkout/:id')
+  checkout(@Param('id') id: string) {
+    return this.gameService.checkout(id);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Server is running on port 3000');
+
+  const config = new DocumentBuilder()
+    .setTitle('CAsino Backend')
+    .setDescription('Challenge to the casino backend')
+    .setVersion('1.0')
+    .addTag('Caino')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  console.log(`Application is running on http://localhost:${PORT}`);
 }
 bootstrap();
